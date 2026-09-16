c26d8516cbba96f724081a267a2735e5581a3247
joselingG <joseling.gonzalez22@outlook.com>
2026-09-16 11:29:42 -0600

    Change path to icons

--- js/module/themes.js
+++ js/module/themes.js
@@ -26,29 +26,29 @@ export function initTheme() {
             if (currentTheme === 'dark') {
                 document.documentElement.setAttribute('data-theme', 'light');
                 fn_changeLogo(logo => {
-                    logo.src = '../assets/logo/joseling-logo.svg';
+                    logo.src = './assets/logo/joseling-logo.svg';
                 });
                 fn_changeModeImg(img => {
-                    img.src = '../assets/icons/icon-sun.svg';
+                    img.src = './assets/icons/icon-sun.svg';
                 });
 
-                iconSearch.src = '../assets/icons/icon-search.svg';
-                iconGithub.src = '../assets/icons/icon-github.svg';
-                iconLinkedin.src = '../assets/icons/icon-linkedin.svg';
-                iconInstagram.src = '../assets/icons/icon-instagram.svg';
+                iconSearch.src = './assets/icons/icon-search.svg';
+                iconGithub.src = './assets/icons/icon-github.svg';
+                iconLinkedin.src = './assets/icons/icon-linkedin.svg';
+                iconInstagram.src = './assets/icons/icon-instagram.svg';
             } else {
                 document.documentElement.setAttribute('data-theme', 'dark');
                 fn_changeLogo(logo => {
-                    logo.src = '../assets/logo/joseling-logo-white.svg';
+                    logo.src = './assets/logo/joseling-logo-white.svg';
                 });
                 fn_changeModeImg(img => {
                     img.src = '../assets/icons/icon-moon.svg';
                 });
 
-                iconSearch.src = '../assets/icons/icon-search-white.svg';
-                iconGithub.src = '../assets/icons/icon-github-white.svg';
-                iconLinkedin.src = '../assets/icons/icon-linkedin-white.svg';
-                iconInstagram.src = '../assets/icons/icon-instagram-white.svg';
+                iconSearch.src = './assets/icons/icon-search-white.svg';
+                iconGithub.src = './assets/icons/icon-github-white.svg';
+                iconLinkedin.src = './assets/icons/icon-linkedin-white.svg';
+                iconInstagram.src = './assets/icons/icon-instagram-white.svg';
             }
             currentTheme = document.documentElement.getAttribute('data-theme');
             localStorage.setItem('theme', currentTheme);
