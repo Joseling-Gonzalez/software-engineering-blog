9876cc29496792fbee7d5a3450e213d79fc446aa
joselingG <joseling.gonzalez22@outlook.com>
2026-09-16 09:51:58 -0600

    Validates path

--- js/layout/footer.js
+++ js/layout/footer.js
@@ -1,5 +1,5 @@
 export async function loadFooter() {
-    const result = await fetch('../components/footer.html');
+    const result = await fetch('./components/footer.html');
     const html = await result.text();
     document.querySelector('#footer').innerHTML = html;
 }
--- js/views/home/home.js
+++ js/views/home/home.js
@@ -2,7 +2,7 @@ import { loadRecentArticles } from "./recent-articles.js";
 import { loadCleanupArticleStyle } from "../../layout/cleanupArticleStyle.js";
 
 export async function loadHome() {
-    const result = await fetch('../view/home/home.html');
+    const result = await fetch('./view/home/home.html');
     const html = await result.text();
     document.querySelector('#main').innerHTML = html;
 
