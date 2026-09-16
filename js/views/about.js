b30afcbc695b0d46cc6ec24e474b399f6b0a1d11
joselingG <joseling.gonzalez22@outlook.com>
2026-09-16 10:04:06 -0600

    Validates path

--- js/views/about.js
+++ js/views/about.js
@@ -1,7 +1,7 @@
-import { loadCleanupArticleStyle } from "./js/layout/cleanupArticleStyle.js";
+import { loadCleanupArticleStyle } from "../../layout/cleanupArticleStyle.js";
 
 export async function loadAbout() {
-    const result = await fetch('../view/about/about.html');
+    const result = await fetch('./view/about/about.html');
     const html = await result.text();
 
     document.querySelector('#main').innerHTML = html;
